10f1982c96947b10c54ee2213b1f7aa66947e0cb
thaint2908 <thai.nt05112000@gmail.com>
2023-02-03 23:02:59 +0700

    fix: 0203

--- src/module/shortPlaylist/shortPlaylist.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { ShortPlayListRepository } from './repository/shortPlayList.repository';
-
-@Injectable()
-export class ShortPlaylistService {
-    constructor(private shortPlayListRepository: ShortPlayListRepository) {}
-}
